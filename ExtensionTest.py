1edee4265b37b1c2fabb17a7ed3399d975b38a83
joanzhu728 <joanzhu728@gmail.com>
2022-08-06 10:35:11 +0900

    VScode Extension Test

---
+++ ExtensionTest.py
@@ -0,0 +1,26 @@
+#Error Lens
+print(a)
+
+#Indent-rainbow
+for _ in range(10):
+    print('hello')
+    for _ in range(20):
+        print('hi')
+        for _ in range(30):
+            print('bye')
+
+#Trailing Spaces
+print('a')  
+print('b')      
+
+#autoDocString
+def myfunc(a,b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+    """
+    print(a)
+    print(b)
+
